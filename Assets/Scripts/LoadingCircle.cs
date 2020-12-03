e9722c622cbd07c00af24f47614ba63aa3087329
James Brooks <james@arajourneys.com>
2020-12-03 16:01:44 +1300

    Landing Page
    Added a radial progress bar and textmesh for tips. There are various ways for loading progress script depending on how heavy resources are. For now using time for progress bar and delay for next scene

---
+++ Assets/Scripts/LoadingCircle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingCircle : MonoBehaviour
+{
+
+    public Transform loadingBar;
+    [SerializeField] private float currentAmount;
+    [SerializeField] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+        }
+        loadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+    }
+
+    //public int sceneID;
+    //public Image loadingImg;
+    ////public Text progressText;
+
+    //void Start()
+    //{
+    //    StartCoroutine(AsyncLoad());
+    //}
+
+    //IEnumerator AsyncLoad()
+    //{
+    //    AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+    //    while (!operation.isDone)
+    //    {
+    //        float progress = operation.progress / 0.9f;
+    //        loadingImg.fillAmount = progress;
+    //        //progressText.text = string.Format("{0:0}%", progress * 100);
+    //        yield return null;
+    //    }
+    //}
+}
---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor.UIElements;
+using System;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public static LoadingScreen instance;
+
+    [SerializeField]
+    private GameObject progressBarHolder;
+
+    [SerializeField]
+    private Image progressBarFill;
+    private float progressValue = 1.1f;
+    public float progressMultiplier1 = 0.3f;
+    public float progressMultiplier2 = 0.08f;
+
+    public float loadLevelTime = 3f;
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadNewLevel());
+    }
+
+    //private string LoadNewLevel()
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        progressBarHolder.SetActive(true);
+        progressValue = 0f;
+
+        //Time.timescale = 0f;
+
+        SceneManager.LoadScene(levelName);
+    }
+
+    void ShowLoadingScreen()
+    {
+        if(progressValue < 1f)
+        {
+            progressValue += progressMultiplier1 * progressMultiplier2;
+            progressBarFill.fillAmount = progressValue;
+
+            // The progress bar has finished
+            if(progressValue >= 1f)
+            {
+                progressValue = 1.1f;
+
+                progressBarFill.fillAmount = 0f;
+                progressBarHolder.SetActive(false);
+
+                //Time.timeScale = 1f;
+            }
+        } // If the progress value is < 1
+    }
+
+    IEnumerator LoadNewLevel()
+    {
+        yield return new WaitForSeconds(loadLevelTime);
+
+        LoadLevel("Whakamahere");
+    }
+
+} // Class
